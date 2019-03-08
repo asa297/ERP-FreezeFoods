647aef43ea1f494a373f3c35c31f9624711aa056
SuperAuMan <makejack4@gmail.com>
2019-03-08 22:05:06 +0700

    test na

--- pages/category/form.js
+++ pages/category/form.js
@@ -10,7 +10,7 @@ import {
 } from "<actions>";
 import { Formik, Field } from "formik";
 import styled from "styled-components";
-import { Router } from "<routes>";
+import { Router } from "../../routes";
 
 class Form extends React.PureComponent {
   state = {
